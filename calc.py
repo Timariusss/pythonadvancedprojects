cd57c3ee1221838fca21ce85a750e74dd6325fb9
Timariusss <neznaya941@gmail.com>
2026-02-09 11:57:03 +0200

    first commit

---
+++ calc.py
@@ -0,0 +1,20 @@
+ch1 = float(input("Первое число: "))
+
+while True:
+    op = input("Операция (+, -, *, /) или 'q' для выхода: ")
+    
+    if op == 'q':
+        break
+    
+    ch2 = float(input("Второе число: "))
+    
+    if op == '+':
+        result = ch1 + ch2
+    elif op == '-':
+        result = ch1 - ch2
+    elif op == '*':
+        result = ch1 * ch2
+    elif op == '/':
+        result = ch1 / ch2
+    
+    print(f"Результат: {result}") 
